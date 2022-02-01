b6d95c5f78099836fd315314a608851a264f6869
LilyCrown999 <lilycrown999@gmail.com>
2022-02-01 13:42:08 +0100

    Responsiveness adjustment

--- src/Browser.tsx
+++ src/Browser.tsx
@@ -1,60 +1,94 @@
 
 import { useState } from "react"
+import ReactDOM from 'react-dom';
 
-function Browser() {
+
+
+    // function name(a: any, b: any) {
+    //     var a = b 
+    //     if (!a.match('^https?:') ) 
+    //         a = 'https://' + a
+    //         return a
+        
+    // }
+
+
+const Browser = (uri: any) => {
+
     const [ url, setUrl] = useState('')
+
+    
+    const link = () => {
+        // var urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
+        var link = url;
+            if (!link.match('^https?:') ) 
+                link = 'https://' + link
+                return link
+        };   
+
+    
+    ReactDOM.render(
+        <div className="container-iframe" >
+       <iframe src={link()} className="responsive-iframe" frameBorder="0"  ></iframe>
+       </div>,
+       document.getElementById('down')
+        
+    )
+    
     const handleSubmit = (e: any) => {
         // console.log(url);
         if (e.code === 'Enter')
           e.preventDefault();
     };
 
-    const uri = "https://" + url; 
 
+  return (
+    <div>
 
-    return(
-                
-        <>
-        <div className='flex-row'>
-        <section className='section'>
-            <h1 className="text-center text-4xl h1 text-blue-700
-                        rounded-l-lg p-4
-                        border-t mr-0 
-                        border-b border-l font italic"> Mini Browser </h1>
-            <div className="text-center
-                px-8 rounded-r-lg text-green-900
-                font italic"> Enter your link without <i className="text-red-500"> "https://" </i>  to display its Webpage </div>
-            <article className=' font-mono text-center container'>
-            <div className =" left-10 right-8 form text-center self-center">
-                <form className=" text-center  rounded-sm space-x-3" >
-                            <label htmlFor="url"> Url: </label>
-                            <input className="
-                            border-2
-                            rounded-l-lg p-2
-                            border-gray-200
-                            bg-white "
-                            type ="text"
-                            id="url" 
-                            name="url" 
-                            value={url}
-                            onChange={(e) => setUrl(e.target.value)}
-                            onClick={handleSubmit}
-                            onKeyDown={handleSubmit}
-                            />
-                    </form> 
+            <div className="App">
+            <div className='flex-row'>
+            <section className='section'>
+                <h1 className="text-center text-4xl h1 text-blue-700
+                            rounded-l-lg p-4
+                            border-t mr-0 
+                            border-b border-l font italic"> Mini Browser </h1>
+                <div className="text-center
+                    px-8 rounded-r-lg text-green-900
+                    font italic"> Test with <i className="text-red-500"> https://asurascans.com </i> or  <i className="text-green-700"> asurascans.com </i> to  preview their website ...</div>
+                <article className=' font-mono text-center container'>
+                <div className =" left-10 right-8 form text-center self-center">
+                    <div className="text-center  rounded-sm space-x-3" style={{ display: "inline-block"}}>
+                        <form className=" flex-col " >
+                                    <label htmlFor="url"> Url: </label>
+                                    <input className="
+                                    border-2
+                                    rounded-l-lg p-2
+                                    border-gray-200
+                                    bg-white "
+                                    type ="text"
+                                    id="url" 
+                                    name="url" 
+                                    value={url}
+                                    onChange={(e) => setUrl(e.target.value)}
+                                    onClick={handleSubmit} onKeyDown={handleSubmit}
+                                    />
+                            </form> 
+
+                    </div>
+                </div>
+                </article>
+            </section>
             </div>
-            </article>
-        </section>
-        <section className="iframe-container">
-            <iframe loading="lazy" src = {uri} className="mini" >
-                    {/* // Webpage shows here */}
-                </iframe>    
-        </section>
-        </div>
-        </>  
-    )
+            </div>
 
-  }
-  
-  export default Browser;
-  
+
+    </div>
+
+
+    
+
+  )
+
+}
+
+export default Browser;
